339c19c27af8a73642406aaebd1ed83d56cc5107
dsuket <dai.takaoka@gmail.com>
2016-03-21 16:29:32 +0900

    apply mvi

--- src/components/kiyoshi.component.ts
+++ src/components/kiyoshi.component.ts
@@ -1,118 +1,114 @@
 import {Component} from "angular2/core";
 import * as Rx from 'rxjs';
-import {ZundokoModel} from "./zundoko.model";
-import {Observable} from "rxjs/Observable";
 
 @Component({
   selector: 'kiyoshi',
   styles: [require('./kiyoshi.component.scss')],
   template:`
-  <div class="kiyoshi-check rank-{{rank}}" (keydown)="keydown($event)" tabindex="1">
+  <div class="kiyoshi-check rank-{{rank}}">
     <h2>キヨシチェック</h2>
     <div class="form-panel">
-      <button (click)="start()">start</button>
-      <button (click)="stop()">stop</button>
+      <button (click)="start($event)">start</button>
+      <button (click)="stop($event)">stop</button>
       Speed: <input type="range" max="1000" min="100" step="10" [(ngModel)]="speed"> {{speed}}
     </div>
     <div class="zundoko-container">
-      <div class="zundoko-item" *ngFor="#item of items">{{item}} </div>
+      <div class="zundoko-item" *ngFor="#item of items$ | async">{{item}} </div>
     </div>
-    <span class="kiyoshi" [hidden]="!kiyoshi">＼キ・ヨ・シ！／</span>
+    <span class="kiyoshi" [hidden]="!showKiyoshi">＼キ・ヨ・シ！／</span>
   </div>
   `,
-  providers: [ZundokoModel]
 })
 
 export class KiyoshiComponent {
-  private items;
-  private kiyoshi;
-  private rank;
-  private speed = 500;
-  private subscriber;
+  // model
+  private zun$ = Rx.Observable.of('ズン');
+  private doko$ = Rx.Observable.of('ドコ');
+  private items$;
+
+  // intent
+  private start$;
+  private stop$;
+
+  // view properties
+  private showKiyoshi = false;
   private subscription;
-  private zundokoSubject;
+  private items = [];
 
-  constructor(private model: ZundokoModel) {
-    this.subscriber = this.createSubscriber();
-    this.start();
+  constructor() {
+    this.start$ = new Rx.Subject();
+    this.stop$ = new Rx.Subject();
+
+    this.start$
+      .startWith({})
+      .subscribe(ev => this.onStart());
+    this.stop$
+      .subscribe(ev => this.onStop());
   }
 
-  keydown(event) {
-    console.log('event', event);
-    switch(event.code) {
-      case 'KeyZ':
-        this.zundokoSubject.next('ズン');
-        break;
-      case 'KeyD':
-        this.zundokoSubject.next('ドコ');
-        break;
-    }
+  createZundokoStream() {
+    const randomStream = Rx.Observable.create(observer => {
+      observer.next(!Math.floor(Math.random()*2));
+      observer.complete();
+    });
+    return randomStream.flatMap(val => (val ? this.zun$ : this.doko$));
   }
 
-  start() {
-    this.kiyoshi = false;
-    this.rank = 0;
-    this.items = [];
-    this.stop();
-    const zundokoStream = this.model.createStream();
-    const kiyoshiStream = this.createIntervalStream()
-      .mergeMap(() => {
-        this.zundokoSubject = new Rx.Subject();
-        return zundokoStream.merge(this.zundokoSubject);
-      })
-      .do(x => this.updateZundoko(x))
+  createKiyoshiStream() {
+    const zundokoStream = this.createZundokoStream();
+    return Rx.Observable.interval(400)
+      .mergeMapTo(zundokoStream)
+      .do(x => this.addItem(x))
       .bufferCount(5, 1)
       .takeWhile(val => val.join('') !== 'ズンズンズンズンドコ');
-
-    this.subscription = kiyoshiStream.subscribe(this.subscriber);
   }
 
-  updateZundoko(x) {
-    this.items.push(x);
-    if (x === 'ドコ') {
-      this.rank = 0;
-    } else {
-      this.rank = this.rank > 3 ? 4 : this.rank + 1;
-    }
+  addItem(item) {
+    this.items.push(item);
+    this.updateItems$();
+  }
+  cleatItems() {
+    this.items = [];
+    this.updateItems$();
   }
 
-  stop() {
+  updateItems$() {
+    const itemStream = Rx.Observable.fromArray(this.items);
+    this.items$ = itemStream.toArray();
+    this.items$.subscribe(x => {
+        console.log('x:', x);
+      });
+  }
+
+  // start handler
+  start($event) {
+    this.start$.next($event);
+  }
+
+  // stop handler
+  stop($event) {
+    this.stop$.next($event);
+  }
+
+  // start subscriber
+  onStart() {
+    this.stop({});
+    this.cleatItems();
+    const kiyoshiStream = this.createKiyoshiStream();
+    this.subscription = kiyoshiStream
+      .subscribe({
+        next: () =>  {},
+        error: err => console.log('Error: ', err),
+        complete: () => this.showKiyoshi = true,
+      });
+  }
+
+  // stop subscriber
+  onStop() {
     if (this.subscription) {
       this.subscription.unsubscribe();
     }
     this.subscription = null;
   }
 
-  createIntervalStream() {
-    const self = this;
-    return Rx.Observable.create(subsriber => {
-      let count = 0;
-      let isStop = false;
-      function timer() {
-        setTimeout(() => {
-          if (isStop) {
-            return;
-          }
-          subsriber.next(count++);
-          console.log('timer', count);
-          timer();
-        }, self.speed)
-      }
-      timer();
-      return function() {
-        isStop = true;
-      };
-    })
-  }
-
-  createSubscriber() {
-    return {
-      next: () => {},
-      error: err => console.error('Error', err),
-      complete: () => {
-        this.kiyoshi = true;
-        this.rank = 5;
-      },
-    };
-  }
 }
